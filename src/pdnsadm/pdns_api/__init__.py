9110cc5eaf15c47f0a150912f5b8749fd1183a61
luto <m@luto.at>
2019-01-19 22:49:49 +0100

    extrace API handling into dedicated module

---
+++ src/pdnsadm/pdns_api/__init__.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+import powerdns
+
+
+def records_axfr(self):
+    """Get zone's records as AXFR"""
+    lines = (r.split('\t') for r in self._get(self.url + '/export')['zone'].strip().split('\n'))
+    return [
+        {'name': r[0], 'ttl': r[1], 'type': r[2], 'content': r[3]}
+        for r in lines
+    ]
+
+powerdns.interface.PDNSZone.records_axfr = records_axfr
+
+class pdns():
+    def __init__(self):
+        api_client = powerdns.PDNSApiClient(
+            api_endpoint=settings.PDNS_APIURL,
+            api_key=settings.PDNS_APIKEY
+        )
+        self.api = powerdns.PDNSEndpoint(api_client)
+
+    @property
+    def server(self):
+        return self.api.servers[0]
+
+__all__ = [
+    'pdns',
+]
