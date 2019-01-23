ab0278170d380e27e24a157168285a19356c4577
luto <m@luto.at>
2019-01-23 09:27:00 +0100

    remove unneeded kwarg from delete zone URL

--- src/pdnsadm/zoneeditor/urls.py
+++ src/pdnsadm/zoneeditor/urls.py
@@ -7,8 +7,8 @@ app_name = 'zoneeditor'
 urlpatterns = [
     path('zones', views.ZoneListView.as_view(), name="zone_list"),
     path('zones/create', views.ZoneCreateView.as_view(), name="zone_create"),
+    path('zones/delete', views.ZoneDeleteView.as_view(), name="zone_delete"),
     path('zones/<zone>', RedirectView.as_view(pattern_name='zoneeditor:zone_records', permanent=False), name="zone_detail"),
     path('zones/<zone>/records', views.ZoneRecordsView.as_view(), name="zone_records"),
     path('zones/<zone>/records/create', views.RecordCreateView.as_view(), name="zone_record_create"),
-    path('zones/<zone>/delete', views.ZoneDeleteView.as_view(), name="zone_delete"),
 ]
