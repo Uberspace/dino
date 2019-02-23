d2b3abf372d9484149795b096ad9c88ea890d9f7
luto <m@luto.at>
2019-02-23 13:23:24 +0100

    introduce $DINO_BASE_DIR to store sqlite-db, static and media files
    close #20

--- src/dino/settings.py
+++ src/dino/settings.py
@@ -16,9 +16,6 @@ import os
 import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 def env(name, default='__unset', kind=str):
     env_name = f'DINO_{name.upper()}'
@@ -40,6 +37,10 @@ def env(name, default='__unset', kind=str):
     else:
         raise Exception(f'Invalid variable type {kind} for {name}/{env_name}.')
 
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+DEFAULT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = env('BASE_DIR', DEFAULT_BASE_DIR)
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG', False, bool)
@@ -173,8 +174,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
 
 
 # Logging
