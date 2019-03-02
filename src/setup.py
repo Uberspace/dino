f3e2554d554cbc09491c1bf8e35bfe4f5b57a089
luto <m@luto.at>
2019-03-02 10:29:18 +0100

    add python3.6 support to setup.py

--- src/setup.py
+++ src/setup.py
@@ -36,6 +36,7 @@ setup(
         'Framework :: Django :: 2.2',
         'Intended Audience :: System Administrators',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
